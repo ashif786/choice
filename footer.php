0ad413039142f02d02e37039a7d16b072292a9e8
choice <mohamedashif786@gmail.com>
2020-01-20 15:30:54 +0400

    file rm frm folder

---
+++ footer.php
@@ -0,0 +1,50 @@
+  
+<div class="container-fluid"><div class="row" style="background: #000000; color: white;padding: 18px;">
+    <div  class="col-sm-4"> 
+ 
+ © 2020 Copyright Choice Auto Rental All Rights Reserved 
+     </div>
+    
+ <div class="col-sm-3 col-sm-offset-5"  >
+     <a href="" style="text-decoration: none;"> <i class="fab fa-facebook-f" style="color: #f5f2f2;">
+              
+           </i> </a>&nbsp; 
+           <a href="" style="text-decoration: none;">
+            <i class="fab fa-twitter" style="color: #f5f2f2;"></i> </a>&nbsp;
+            <a href="" style="text-decoration: none;">
+           <i class="fab fa-instagram" style="color: #f5f2f2;"></i> </a>&nbsp;
+           <a href="" style="text-decoration: none;">
+           <i class="fab fa-linkedin-in" style="color: #f5f2f2;"></i> </a>&nbsp;
+           <a href="" style="text-decoration: none;">
+           <i class="fab fa-foursquare" style="color: #f5f2f2;"></i> </a>&nbsp;
+           <a href="" style="text-decoration: none;">
+           <i class="fab fa-pinterest" style="color: #f5f2f2;"></i> </a>&nbsp;
+           <a href="" style="text-decoration: none;">
+           <i class="fab fa-yelp" style="color: #f5f2f2;"></i> </a>&nbsp;
+           <a href="" style="text-decoration: none;">
+           <i class="fab fa-youtube" style="color: #f5f2f2;"></i> </a>
+        <span><img style="width: 25%;  margin: 0 -18px;" src="img/logo/liberty.png" alt=""/></span>
+    </div>
+        
+     
+</div>
+</div>
+<script>
+ $(document).ready(function(){
+var btn = $('#button');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+});
+ });
+</script>
+ 
